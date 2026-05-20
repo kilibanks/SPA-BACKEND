391fd9abf614e36a58a05f54383aea6fc318a852
Vincent Ogweno <ogwenovincent00@gmail.com>
2026-05-21 00:18:34 +0300

    enforced ipv4...

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -1,21 +1,24 @@
-// config/mailer.js
 const nodemailer = require("nodemailer");
 const dns = require("dns");
 
 dns.setDefaultResultOrder("ipv4first");
 
-
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+
+  family: 4, // IMPORTANT
+
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
   },
+
   tls: {
     rejectUnauthorized: false,
   },
+
   socketTimeout: 10000,
   greetingTimeout: 10000,
   connectionTimeout: 10000,
